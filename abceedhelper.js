548be51a8dfec3da54d850c9a0fb91ba1ffd17db
namaste59 <wk199859@gmail.com>
2023-03-31 17:09:17 +0900

    不要な部分の削除・コメント追加

--- abceedhelper.js
+++ abceedhelper.js
@@ -1,3 +1,4 @@
+//ウィンドウの読み込みのために3秒待つ
 setTimeout(main = () => {
     const bottuns = {
         rewind : document.querySelectorAll('.sound-controller_item')[0],
@@ -28,37 +29,3 @@ setTimeout(main = () => {
     window.addEventListener('keydown', pushBottun);
 }
 ,3000);
-
-// window.addEventListener("load", main);
-// function main(){
-//     bottuns = {};
-//     GetElements(bottuns);
-//     console.log(bottuns.rewind);
-//     window.addEventListener('keydown', (e) => {
-//         if (e.keyCode === 37){
-//             bottuns.rewind3.click();
-//         }
-//         if (e.keyCode === 39){
-//             bottuns.forward3.click();
-//         }
-//         if (e.keyCode === 32) {
-//             bottuns.play.click();
-//         }
-//         if (e.keyCode === 37 && e.shiftKey === true) {
-//             bottuns.rewind.click();
-//         }
-//         if (e.keyCode === 39 && e.shiftKey === true) {
-//             bottuns.forward.click();
-//         }
-//     });
-// }
-// function GetElements(obj) {
-//     return obj = {
-//         rewind : document.querySelectorAll('.sound-controller_item')[0],
-//         rewind3 : document.querySelectorAll('.sound-controller_item')[1],
-//         play : document.querySelectorAll('.sound-controller_item')[2],
-//         forward3 : document.querySelectorAll('.sound-controller_item')[3],
-//         forward : document.querySelectorAll('.sound-controller_item')[4],
-//     }
-// }
-
